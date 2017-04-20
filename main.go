a5af699df9976b6a2069618d174e42d1864fedb3
Joe Elliott <jelliott@qsrautomations.com>
2017-04-19 21:28:30 -0500

    Basic webserver

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
+type msg struct {
+	Message string `json:"msg"` //= {"msg":"Hi"}
+}
+
 func main() {
-	fmt.Println("Hello, playground")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := json.NewEncoder(w).Encode(&msg{Message: "Hello Golang UK Conf"}); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
