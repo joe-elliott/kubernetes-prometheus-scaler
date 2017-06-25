4ff0759a5f673d36c63723306b448f99c05dd3e8
Joe Elliott <jelliott@qsrautomations.com>
2017-06-25 11:52:09 -0400

    Fixed pod killing issue.  Conditionally start minikube

--- debug.sh
+++ debug.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+# if minikube isn't running, start it
+minikube ip
+if [ $? -ne 0 ]; then
+  minikube start
+fi
+
+# sets the docker env to point at minikube so the images we build will be available in minikube
 eval $(minikube docker-env)
 
+# build the debug image
 docker build . -t go-app:debug -f debug.Dockerfile
 
-kubectl replace -f ./debug.podspec.yml --force
+# delete old stuff
+kubectl delete po go-debug
+kubectl delete svc go-debug-svc
 
+#wait for stuff to be deleted
+while kubectl get po go-debug > /dev/null; do :; done
+while kubectl get svc go-debug-svc > /dev/null; do :; done
+
+# make the new stuff
+kubectl create -f ./debug.podspec.yml
+
+# display to the user the endpoints to put in launch.json
 minikube service go-debug-svc --url
