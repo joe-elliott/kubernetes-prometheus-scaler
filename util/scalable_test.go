b7153c236145e6aa726dfb0e78d765fdd958add5
Joe Elliott <jelliott@qsrautomations.com>
2017-07-18 21:54:15 -0400

    Add testing structure

--- util/scalable_test.go
+++ util/scalable_test.go
@@ -6,31 +6,59 @@ import (
 	"github.com/Knetic/govaluate"
 )
 
+type testCase struct {
+	description   string
+	expectedScale int64
+	scalable      Scalable
+}
+
+var _trueExpr, _ = govaluate.NewEvaluableExpression("true")
+var _falseExpr, _ = govaluate.NewEvaluableExpression("false")
+
+var _testCases = []testCase{
+	{
+		description:   "Will Scale Up",
+		expectedScale: 4,
+		scalable: &StepScalable{
+			BaseScalable: BaseScalable{
+				query:    "query",
+				minScale: 2,
+				maxScale: 4,
+				curScale: 3,
+			},
+			scaleUpWhen:   _trueExpr,
+			scaleDownWhen: _falseExpr,
+		},
+	},
+	{
+		description:   "Will Scale Down",
+		expectedScale: 2,
+		scalable: &StepScalable{
+			BaseScalable: BaseScalable{
+				query:    "query",
+				minScale: 2,
+				maxScale: 4,
+				curScale: 3,
+			},
+			scaleUpWhen:   _falseExpr,
+			scaleDownWhen: _trueExpr,
+		},
+	},
+}
+
 func TestStepScalable(t *testing.T) {
 
-	trueExpr, _ := govaluate.NewEvaluableExpression("true")
-	falseExpr, _ := govaluate.NewEvaluableExpression("false")
+	for _, testCase := range _testCases {
 
-	s := StepScalable{
-		BaseScalable: BaseScalable{
-			query:    "query",
-			minScale: 2,
-			maxScale: 5,
-			curScale: 2,
-		},
-		scaleUpWhen:   trueExpr,
-		scaleDownWhen: falseExpr,
-	}
-
-	t.Run("Will Scale Up", func(t *testing.T) {
-		newScale, err := CalculateNewScale(&s, 0.0)
+		newScale, err := CalculateNewScale(testCase.scalable, 0.0)
 
 		if err != nil {
 			t.Errorf("Unexpected error: %v", err)
 		}
 
-		if newScale != s.curScale+1 {
-			t.Error("Did not scale up.")
+		if newScale != testCase.expectedScale {
+			t.Errorf("Test [%v] failed.", testCase.description)
 		}
-	})
+	}
+
 }
