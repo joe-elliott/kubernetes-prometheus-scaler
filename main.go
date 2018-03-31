8e138be554dfa5d8d3150e40e7b2952b540fac52
Joe Elliott <jelliott@qsrautomations.com>
2018-03-31 08:29:57 -0400

    Updated glide deps

--- main.go
+++ main.go
@@ -12,47 +12,64 @@ import (
 	"k8s.io/client-go/rest"
 
 	"github.com/op/go-logging"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"kubernetes-prometheus-scaler/util"
 )
 
-const deploymentLabelSelector = "scale==prometheus"
-
-var log = logging.MustGetLogger("prometheus-autoscaler")
-
-var format = logging.MustStringFormatter(
-	`%{time:15:04:05.000} %{level:.4s} %{message}`,
+const (
+	deploymentLabelSelector = "scale==prometheus"
+	prometheusNamespace     = "prometheusscaler"
 )
 
-var prometheusURL = flag.String("prometheus-url", "http://prometheus:9090", "URL to query.")
-var assessmentInterval = flag.Duration("assessment-interval", 60*time.Second, "Time to sleep between checking deployments.")
+var (
+	_log       = logging.MustGetLogger("prometheus-autoscaler")
+	_logFormat = logging.MustStringFormatter(
+		`%{time:15:04:05.000} %{level:.4s} %{message}`,
+	)
+
+	_prometheusURL      = flag.String("prometheus-url", "http://prometheus:9090", "URL to query.")
+	_assessmentInterval = flag.Duration("assessment-interval", 60*time.Second, "Time to sleep between checking deployments.")
+
+	_errorTotal = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: "error",
+			Name:      "total",
+			Help:      "Kubernetes Prometheus Scaler Errors",
+		},
+	)
+)
 
 func main() {
 
 	flag.Parse()
 
 	backend := logging.NewLogBackend(os.Stdout, "", 0)
-	backendFormatted := logging.NewBackendFormatter(backend, format)
+	backendFormatted := logging.NewBackendFormatter(backend, _logFormat)
 
 	logging.SetBackend(backendFormatted)
 
-	log.Infof("prometheus-url=%v", *prometheusURL)
-	log.Infof("assessment-interval=%v", *assessmentInterval)
+	_log.Infof("prometheus-url=%v", *_prometheusURL)
+	_log.Infof("assessment-interval=%v", *_assessmentInterval)
 
-	prometheusQuery, err := util.MakePrometheusQueryFunc(*prometheusURL)
+	prometheusQuery, err := util.MakePrometheusQueryFunc(*_prometheusURL)
 	if err != nil {
-		log.Criticalf("makeQueryFunc failed: %v", err)
+		_log.Criticalf("makeQueryFunc failed: %v", err)
+		os.Exit(1)
 	}
 
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Criticalf("rest.InClusterConfig failed: %v", err)
+		_log.Criticalf("rest.InClusterConfig failed: %v", err)
+		os.Exit(1)
 	}
 	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Criticalf("kubernetes.NewForConfig failed: %v", err)
+		_log.Criticalf("kubernetes.NewForConfig failed: %v", err)
+		os.Exit(1)
 	}
 
 	for {
@@ -61,58 +78,58 @@ func main() {
 			LabelSelector: deploymentLabelSelector,
 		})
 		if err != nil {
-			log.Errorf("list deployments: %v", err)
+			_log.Errorf("list deployments: %v", err)
 			continue
 		}
 
-		log.Infof("Considering %d deployments for scaling.", len(deployments.Items))
+		_log.Infof("Considering %d deployments for scaling.", len(deployments.Items))
 
 		for _, deployment := range deployments.Items {
 
-			log.Infof("Considering: %v", deployment.Name)
+			_log.Infof("Considering: %v", deployment.Name)
 
 			scalable, err := util.NewScalable(deployment)
 
 			if err != nil {
-				log.Errorf("NewScalable: %v", err)
+				_log.Errorf("NewScalable: %v", err)
 				continue
 			}
 
-			log.Debugf("Scalable: %+v", scalable)
+			_log.Debugf("Scalable: %+v", scalable)
 
 			// get and evaluate promQuery
 			result, err := prometheusQuery(scalable.GetQuery())
 
 			if err != nil {
-				log.Errorf("prometheusQuery: %v", err)
+				_log.Errorf("prometheusQuery: %v", err)
 				continue
 			}
 
-			log.Infof("result: %f", result)
+			_log.Infof("result: %f", result)
 
 			newScale, err := util.CalculateNewScale(scalable, result)
 
 			if err != nil {
-				log.Errorf("scalingFunc: %v", err)
+				_log.Errorf("scalingFunc: %v", err)
 				continue
 			}
 
 			// set the replica set
 			if scalable.GetCurScale() != newScale {
-				log.Infof("Setting replica count to %d\n", newScale)
+				_log.Infof("Setting replica count to %d\n", newScale)
 				jsonPatch := "[{\"op\": \"replace\", \"path\": \"/spec/replicas\", \"value\": " + strconv.FormatInt(newScale, 10) + " }]"
-				log.Debugf("Patch string: %v\n", jsonPatch)
+				_log.Debugf("Patch string: %v\n", jsonPatch)
 				_, err = clientset.Extensions().Deployments(deployment.Namespace).Patch(deployment.Name, api.JSONPatchType, []byte(jsonPatch))
 
 				if err != nil {
-					log.Errorf("  Error scaling: %v", err)
+					_log.Errorf("  Error scaling: %v", err)
 					continue
 				}
 			} else {
-				log.Infof("curScale == newScale.  Not scaling %v", deployment.Name)
+				_log.Infof("curScale == newScale.  Not scaling %v", deployment.Name)
 			}
 		}
 
-		time.Sleep(*assessmentInterval)
+		time.Sleep(*_assessmentInterval)
 	}
 }
